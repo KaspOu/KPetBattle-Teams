9d39996a83f1e7f5cc69cabc7ba7f32e1e43cc95
Ryad M <ryad@ryme-dev.com>
2024-06-06 14:26:16 +0200

    Reapply original fix

--- PetBattleTeams/modules/GUI/MainFrame.lua
+++ PetBattleTeams/modules/GUI/MainFrame.lua
@@ -71,7 +71,8 @@ local function SetComponentPoints(self, showSelectedTeam, showControls, showRost
 
     local minSize = (showSelectedTeam and 70 or 0) + (showControls and 45 or 0) + (showRoster and ROW_HEIGHT*2 + 30 or 0)
     minSize = (minSize == 0) and 50 or (minSize + self.resizer:GetHeight())
-    self:SetMinResize(self:GetWidth(), minSize)
+    -- self:SetMinResize(self:GetWidth(), minSize)
+    self:SetResizeBounds(self:GetWidth(), minSize)
     if self:GetHeight() < minSize then
         self:SetHeight(minSize)
     end
@@ -134,15 +135,16 @@ local function OnMouseWheel(self,delta)
 end
 
 function GUI:CreateMainFrame()
-    local widget = CreateFrame("frame", "PetBattleTeamFrame", UIParent)
+    local widget = CreateFrame("frame", "PetBattleTeamFrame", UIParent, BackdropTemplateMixin and "BackdropTemplate")
 
     widget:SetClampedToScreen(true)
     widget:SetFrameStrata("MEDIUM")
     widget:SetToplevel(true)
     widget:RegisterForDrag("LeftButton")
     widget:SetSize(165, ROW_HEIGHT * 8 + 166)
-    widget:SetMaxResize(widget:GetWidth(), ROW_HEIGHT * 11 + 150)
-    widget:SetMinResize(widget:GetWidth(), ROW_HEIGHT * 2 + 150)
+    -- widget:SetMaxResize(widget:GetWidth(), ROW_HEIGHT * 11 + 150)
+    -- widget:SetMinResize(widget:GetWidth(), ROW_HEIGHT * 2 + 150)
+    widget:SetResizeBounds(widget:GetWidth(), ROW_HEIGHT * 2 + 150, widget:GetWidth(), ROW_HEIGHT * 11 + 150)
     widget:SetMovable(true)
     widget:EnableMouse(true)
 
